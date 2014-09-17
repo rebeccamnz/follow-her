9483e614b75fa464f0f0f6a530f1a45332ecc960
rebeccamnz <rebeccamunz@MacBook-Air-de-Rebecca.local>
2014-09-17 17:51:55 +0200

    histoire
    imae

---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title><?php echo $title ?></title>
+  <meta name="description" content="<?php echo $meta_description; ?>">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+  <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+  <link href='http://fonts.googleapis.com/css?family=Dosis:500' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=PT+Sans+Narrow' rel='stylesheet' type='text/css'>
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- Leave those next 4 lines if you care about users using IE8 -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  <link type="text/css" href="style.css" rel="stylesheet">
+</head>
+
+<body class="filter">
+
+<div class="wrapper">
+
+<header>
+  <h1>Je vous ai compris !</h1>
+  <img class="rounded" id="logo" src="http://img4.wikia.nocookie.net/__cb20120810131800/nonciclopedia/images/0/05/Lhooq_Duchamp.jpg" alt="Duchamp" />
+  <h2>L'Histoire, <em>autrement</em></h2>
+  <nav>
+      <ul>
+        <a href=#> <li>Reines, Rois et Dirigeants </li> </a>
+        <a href=#> <li>Bataille </li> </a>
+        <a href=#> <li>L'inconnu du coin</li> </a>
+        <a href=#> <li>Le quizz du héros</li> </a>
+      </ul>
+  </nav>
+</header>
